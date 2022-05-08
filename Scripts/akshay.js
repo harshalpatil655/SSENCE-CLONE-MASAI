244482fa67340b35e4bf66f9b78ec374516e2771
your github user nameharshalpatil655 <your github emailharshalpatil655@gmail.com>
2022-05-08 18:23:50 +0530

    added dig

--- Scripts/akshay.js
+++ Scripts/akshay.js
@@ -9,3 +9,12 @@ document.getElementById("firstSecBox").addEventListener("click",secBox)
 function secBox(){
     window.location.href="secBox.html"
 }
+
+document.getElementById("view").addEventListener("click",()=>{
+    zed();
+})
+
+
+let zed= ()=>{
+    window.location.href="/SSENCE-CLONE-MASAI/homedigvijay.html"
+}
